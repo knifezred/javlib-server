424801b90faafd2c0dab9f4eda8f50e50848819a
KnifeZ <knifez@qq.com>
2025-06-27 14:22:32 +0800

    feat(存储): 添加登录接口用于验证密码，如果密码不存在则自动创建

--- src/database/repository/storage.ts
+++ src/database/repository/storage.ts
@@ -5,6 +5,32 @@ import { Storage } from '../entity/storage'
 export function initStorageApi(server: Express) {
   const repository = AppDataSource.getRepository(Storage)
 
+  // 登录接口
+  server.get('/api/login/:password', async (req, res) => {
+    console.log(req.url)
+    try {
+      const result = await repository.findOneBy({ key: 'LOGIN_PASS' })
+      console.log(result)
+      if (result !== undefined && result !== null) {
+        if (result.value === req.params.password) {
+          res.status(200).json(true)
+          return
+        }
+      } else {
+        await repository.save({
+          key: 'LOGIN_PASS',
+          value: req.params.password,
+          createdTime: Date.now()
+        })
+        res.status(200).json(true)
+        return
+      }
+      res.status(200).json(false)
+    } catch (error) {
+      res.status(500).send(error)
+    }
+  })
+
   server.post('/api/storage/list', async (req, res) => {
     console.log(req.url)
     try {
